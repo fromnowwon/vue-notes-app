3c08a7ab22ee3767c1f17b14831f40e0f4721168
cw <fromnowwon@gmail.com>
2024-06-23 21:20:50 +0900

    feat: 날짜 포맷 변경

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,12 +13,27 @@ const emits = defineEmits(["delete"]);
 const deleteNote = () => {
 	emits("delete", props.note.id);
 };
+
+const formatDate = (date) => {
+	const options = {
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+		hour12: false,
+		timeZone: "Asia/Seoul",
+	};
+
+	return new Intl.DateTimeFormat("ko-KR", options).format(date);
+};
 </script>
 
 <template>
 	<div class="card">
 		<p class="main-text">{{ note.text }}</p>
-		<p class="date">{{ note.date }}</p>
+		<p class="date">{{ formatDate(note.date) }}</p>
 		<button class="delete-btn" @click="deleteNote">X</button>
 	</div>
 </template>
