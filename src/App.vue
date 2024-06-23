65c0b0ef9f4baf3d5e78f18ae8493d0bc9852646
cw <fromnowwon@gmail.com>
2024-06-23 20:20:39 +0900

    feat: 메모 추가 기능

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,33 @@
 import { ref } from "vue";
 
 const showModal = ref(false);
+const notes = ref([]);
+const newNote = ref("");
+
+const addNote = () => {
+	notes.value.push({
+		id: Math.floor(Math.random() * 1000000),
+		text: newNote.value,
+		date: new Date(),
+	});
+	showModal.value = false;
+	newNote.value = "";
+};
 </script>
 
 <template>
 	<main>
 		<div class="overlay" v-show="showModal">
 			<div class="modal">
-				<textarea name="note" id="note" rows="10" cols="30"></textarea>
+				<textarea
+					v-model.trim="newNote"
+					name="note"
+					id="note"
+					rows="10"
+					cols="30"
+				></textarea>
 				<div class="btn-box">
-					<button class="add-btn">추가</button>
+					<button class="add-btn" @click="addNote">추가</button>
 					<button class="close-btn" @click="showModal = false">취소</button>
 				</div>
 			</div>
@@ -21,9 +39,9 @@ const showModal = ref(false);
 				<button @click="showModal = true">+</button>
 			</header>
 			<div class="cards-container">
-				<div class="card">
-					<p class="main-text"></p>
-					<p class="date"></p>
+				<div class="card" v-for="note in notes" :key="note.id">
+					<p class="main-text">{{ note.text }}</p>
+					<p class="date">{{ note.date }}</p>
 				</div>
 			</div>
 		</div>
