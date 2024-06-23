842c4e54bd4bea2b8f862a9126bc898c58539bb4
cw <fromnowwon@gmail.com>
2024-06-23 20:15:51 +0900

    feat: 모달 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<template></template>
+const showModal = ref(false);
+</script>
+
+<template>
+	<main>
+		<div class="overlay" v-show="showModal">
+			<div class="modal">
+				<textarea name="note" id="note" rows="10" cols="30"></textarea>
+				<div class="btn-box">
+					<button class="add-btn">추가</button>
+					<button class="close-btn" @click="showModal = false">취소</button>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<header>
+				<h1>Notes</h1>
+				<button @click="showModal = true">+</button>
+			</header>
+			<div class="cards-container">
+				<div class="card">
+					<p class="main-text"></p>
+					<p class="date"></p>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
 
 <style scoped></style>
