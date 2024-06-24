a57d04d67326ce4757e017f8d8617947fd98ad97
cw <fromnowwon@gmail.com>
2024-06-24 13:14:32 +0900

    style: 모달 스타일 추가

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -33,12 +33,15 @@ const closeModal = () => {
 <template>
 	<div class="overlay" v-show="showModal">
 		<div class="modal">
+			<h2 class="tit">새 메모</h2>
 			<textarea
 				v-model.trim="newNote"
 				name="note"
 				id="note"
+				class="note-area"
 				rows="10"
 				cols="30"
+				placeholder="내용을 입력해주세요"
 			></textarea>
 			<p class="error-message" v-if="errorMessage">{{ errorMessage }}</p>
 			<div class="btn-box">
@@ -48,3 +51,63 @@ const closeModal = () => {
 		</div>
 	</div>
 </template>
+
+<style scoped>
+.overlay {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 10;
+}
+
+.modal {
+	width: 400px;
+	padding: 1rem;
+	border-radius: 5px;
+	background-color: #fff;
+	box-shadow: 1px 2px 10px 1px rgba(0, 0, 0, 0.3);
+}
+
+.tit {
+	margin-bottom: 5px;
+	font-size: 1rem;
+}
+
+.note-area {
+	width: 100%;
+	padding: 10px;
+	border: 1px solid #bbb;
+	border-radius: 5px;
+}
+
+.btn-box {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: 5px;
+}
+
+.btn-box button {
+	border: 0;
+	font-size: 0.875rem;
+	margin: 0 5px;
+	padding: 4px 14px;
+	border-radius: 3px;
+	cursor: pointer;
+}
+
+.add-btn {
+	background-color: #333;
+	color: #fff;
+}
+
+.close-btn {
+	background-color: #e5e5e5;
+}
+</style>
